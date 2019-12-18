1255f8ab28a34aabc2946eb229fb7abad1788ee3
XiaoKai <767713227@qq.com>
2019-12-18 17:14:27 +0800

    第一次提交，没有add选项

---
+++ GitAndSourceTreeDemo/GitAndSourceTreeDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GitAndSourceTreeDemo
+//
+//  Created by 王恒凯 on 2019/12/18.
+//  Copyright © 2019 Defence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ GitAndSourceTreeDemo/GitAndSourceTreeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitAndSourceTreeDemo
+//
+//  Created by 王恒凯 on 2019/12/18.
+//  Copyright © 2019 Defence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
